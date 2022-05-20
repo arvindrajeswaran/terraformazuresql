725e20f61e0dfd2b320b0fa44c223449cb035677
Arvind Rajeswaran <arajeswaran@microsoft.com>
2022-05-19 23:35:06 -0400

    primary sql server with 1 sql db

--- main.tf
+++ main.tf
@@ -17,3 +17,48 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
+
+resource "azurerm_storage_account" "example" {
+  name                     = var.saname
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_mssql_server" "example" {
+  name                         = var.sqlname
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = var.sqlusername
+  administrator_login_password = var.sqlpassword
+}
+
+resource "azurerm_mssql_database" "test" {
+  name           = var.databasename
+  server_id      = azurerm_mssql_server.example.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  #license_type   = "LicenseIncluded"
+  max_size_gb    = 250
+  read_scale     = false
+  #sku_name       = "S2"
+  zone_redundant = true
+  tags = {
+    Environment = "AzureSQL-Terraform"
+  }
+    threat_detection_policy { //enable Microsoft Defender for SQL 
+    state                      = "Enabled"
+    storage_endpoint = azurerm_storage_account.example.primary_blob_endpoint
+    storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  }
+}
+/*
+resource "azurerm_mssql_database_extended_auditing_policy" "example" {
+  database_id                             = azurerm_mssql_database.test.id
+  storage_endpoint                        = azurerm_storage_account.example.primary_blob_endpoint
+  storage_account_access_key              = azurerm_storage_account.example.primary_access_key
+  storage_account_access_key_is_secondary = false
+  retention_in_days                       = 6
+}
+*/
--- variables.tf
+++ variables.tf
@@ -5,3 +5,24 @@ variable "resource_group_name" {
 variable "location" {
   type = string
 }
+
+variable "saname" {
+  type = string
+}
+
+variable "sqlname" {
+  type = string
+}
+
+variable "sqlusername" {
+  type = string
+}
+
+variable "sqlpassword" {
+  type = string
+  sensitive = true
+}
+
+variable "databasename" {
+  type = string
+}
